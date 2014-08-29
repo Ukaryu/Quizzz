8bb7e06fc20b4e1d765dd80d8475257884cd1e93
Ukaryu <shark_mario@hotmail.com>
2014-08-29 21:10:24 +0200

    More questions

--- Quiz/src/com/example/quiz/start.java
+++ Quiz/src/com/example/quiz/start.java
@@ -35,9 +35,11 @@ public class start {
         int sporsmaal = tast.inInt();
         if (sporsmaal == 1) {
             skjerm.outln("Korrekt");
+            poeng++;
         }
         else {
             skjerm.outln("Seriøst?");
+            poeng++;
         }
         skjerm.outln();
 
@@ -46,9 +48,11 @@ public class start {
         String kongen = tast.inLine();
         if (kongen.equals("Harald")) {
             skjerm.outln("Korrekt");
+            poeng++;
         }
         else if (kongen.equals("harald")) {
             skjerm.outln("Korrekt");
+            poeng++;
         }
         else {
             skjerm.outln("Dårligt");
@@ -60,9 +64,13 @@ public class start {
         String latin = tast.inLine();
         if (latin.equals("Canis Lupus Familiaris")) {
             skjerm.outln("Meget bra");
+            poeng++;
+            poeng++;
         }
         else if (latin.equals("canis lupus familiaris")) {
             skjerm.outln("Meget bra");
+            poeng++;
+            poeng++;
         }
         else {
             skjerm.outln("Close, but no cigar, more like completely opposite direction of it");
@@ -70,18 +78,31 @@ public class start {
         skjerm.outln();
 
 
-        skjerm.outln("Hva heter kongen av Norge?: ");
-        String kongen = tast.inLine();
-        if (kongen.equals("Harald")) {
+        skjerm.outln("Hva heter HiB på engelsk?: ");
+        String hib = tast.inLine();
+        if (hib.equals("Bergen University College")) {
             skjerm.outln("Korrekt");
         }
-        else if (kongen.equals("harald")) {
+        else if (hib.equals("bergen university college")) {
             skjerm.outln("Korrekt");
+            poeng++;
         }
         else {
             skjerm.outln("Dårligt");
+
         }
         skjerm.outln();
+
+
+        skjerm.outln("Din totale poengsum er " + poeng + " ut av maks 6");
+        skjerm.outln();
+        if (poeng <= 6){
+            skjerm.outln("Full pott, you make me proud son");
+        }
+        else if (poeng <= 4) {
+            skjerm.outln("Du kan sikker gjøre bedre, kanskje");
+        }
+
 
 
 
